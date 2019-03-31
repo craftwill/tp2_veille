6bf87ffa6944a0b4a1c73e4624cedc86d4614d8b
William <43345835+production-jeu@users.noreply.github.com>
2019-03-31 14:18:05 -0400

    Image menu burger ajoutée

--- js/menuBurger.js
+++ js/menuBurger.js
@@ -1,4 +1,9 @@
 window.addEventListener('load',function(){
     console.log('menu burger');
-    
+    let menuBurger = document.getElementsByClassName("menuBurger")[0];
+
+    menuBurger.addEventListener("click", function(e){
+        
+    });
+
 });
